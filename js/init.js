43b11288beef4328114e7787db5569eb0702a656
Spoutnik97 <30442116+Spoutnik97@users.noreply.github.com>
2017-09-04 22:15:30 +0200

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+	$(".dropdown-button").dropdown();
+    $('.scrollspy').scrollSpy();
+	$('.collapsible').collapsible();   
+	$('.modal').modal();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
